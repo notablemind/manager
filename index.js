27572e01501a6137d58c012c141bf08e7e964738
Jared Forsyth <jared@jaredforsyth.com>
2013-12-14 16:17:58 -0700

    refactor. more general and better

--- index.js
+++ index.js
@@ -1,56 +1,94 @@
 
-var _ = require('lodash')
-
-  , utils = require('./utils')
-  , BaseManager = require('./base')
-
 module.exports = Manager
 
-function newId(ln) {
-  var chars = 'abcdef01245689'
-    , id = ''
-  ln = ln || 8
-  for (var i=0; i<chars.length; i++) {
-    id += chars[parseInt(chars.length * Math.random())]
-  }
-  return id
+function Manager() {
+  this._pending = {}
+  this._map = {}
+  this._on = {}
 }
 
-function Manager(data) {
-  BaseManager.call(this)
-  if (data) this.dump(data)
-}
-
-Manager.prototype = _.extend(BaseManager.prototype, {
-  models: ['children', 'data'],
-  newNode: function (data, children) {
-    var id = newId(16)
-    this._map['children'][id] = children || []
-    this._map['data'][id] = data || {}
+Manager.prototype = {
+  defaultNode: {},
+  newNode: function (data) {
+    var id = this.genId()
+    this._map[id] = _.extend({}, data, this.defaultNode)
     return id
   },
-  dump: function (data) {
-    var map = utils.toMap(data)
-    for (var id in map) {
-      this.got('children', id, map[id].children)
-      this.got('data', id, map[id].data)
+  on: function (id, attr, handler) {
+    if (handler === undefined) {
+      handler = attr
+      attr = null
+    }
+    if (!this._on[id]) {
+      this._on[id] = []
+    }
+    if (attr) {
+      this._on[id].push([handler, attr])
+    } else {
+      this._on[id].push(handler)
+    }
+    if (this._map[id]) return handler(attr ? this._map[id][attr] : this._map[id])
+    if (this._pending[id]) return
+    this.fetch(id)
+  },
+  off: function (id, attr, handler) {
+    if (!this._on[id]) return false
+    if (handler === undefined) {
+      handler = attr
+      attr = null
+    }
+    var idx
+    if (!attr) {
+      idx = this._on[id].indexOf(handler)
+      if (idx === -1) return false
+      this._on[id].splice(idx, 1)
+      return true
+    }
+    for (var i=0; i<this._on[id].length; i++) {
+      if (this._on[id][i][0] === handler && this._on[id][i][1] === attr) {
+        this._on[id].splice(i, 1)
+        return true
+      }
+    }
+    return false
+  },
+  set: function (id, attr, data) {
+    this.setter(id, attr, data, function (err, data) {
+      this.got(id, data)
+    }.bind(this))
+  },
+  fetch: function (id) {
+    this._pending[id] = true
+    this.getter(id, function (err, data) {
+      this._pending[id] = false
+      if (err) return this.handleError(err, id)
+      this.got(id, data)
+    }.bind(this))
+  },
+  got: function (id, data) {
+    this._map[id] = data
+    if (!this._on[id]) return
+    for (var i=0; i<this._on[id].length; i++) {
+      this._on[id][i](data)
     }
   },
-  getters: {
-    children: function (id, done) {
-      done(null, [])
-    },
-    data: function (id, done) {
-      done(null, {})
+  setter: function (id, attr, data, done) {
+    if (data === undefined) {
+      data = attr
+      attr = null
+      this._map[id] = data
+    } else {
+      if (!this._map[id]) this._map[id] = {}
+      this._map[id][attr] = data
     }
+    done(null, this._map[id])
   },
-  setters: {
-    children: function (id, children, done) {
-      done(null, children)
-    },
-    data: function (id, data, done) {
-      done(null, data)
-    }
+  getter: function (id, done) {
+    done(null, {})
+  },
+  handleError: function (err, id) {
+    console.error('Failed to fetch', id, err, err.message)
   }
-})
+}
+
 
